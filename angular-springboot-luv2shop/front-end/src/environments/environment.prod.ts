f32d26e70a4f98f32bfcff9e32632f759207ecde
cgray09 <47266486+cgray09@users.noreply.github.com>
2023-02-19 18:23:04 -0800

    Add files via upload

---
+++ angular-springboot-luv2shop/front-end/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  luv2shopApiUrl: "https://luv2shop-ecommerce.herokuapp.com/api",
+  stripePublishableKey: "pk_test_51HEdWoCf4RBlO79Rg1QklXntzVcq1UeEH7VXBE1bUk3VWJb5uNzAPmlKaVSFf1jcXgvB2rSWihRGfogUQbH9lBNZ00lKRMyMtA"
+};
