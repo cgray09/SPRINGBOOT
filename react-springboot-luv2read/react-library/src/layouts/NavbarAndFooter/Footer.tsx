9845dc328d3a79647f0067015dd06d0b37271fb4
cgray09 <47266486+cgray09@users.noreply.github.com>
2023-02-28 09:13:11 -0800

    Add files via upload

---
+++ react-springboot-luv2read/react-library/src/layouts/NavbarAndFooter/Footer.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+    return (
+        <div className='main-color'>
+            <footer className='container d-flex flex-wrap 
+                justify-content-between align-items-center py-5 main-color'>
+                <p className='col-md-4 mb-0 text-white' > 
+                    <div className="navtag footer">
+                        <span className="m span-style">Website Maker: Christian V. Grayson</span>
+                        <span className="m">Terms of Service</span>
+                        <span className="m">© Privacy</span>
+                    </div>
+                </p>
+                
+                <ul className='nav navbar-dark col-md-4 justify-content-end'>
+                    <li className='nav-item'>
+                        <Link to='/home' className='nav-link px-2 text-white'>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/search' className='nav-link px-2 text-white'>
+                            Search Books
+                        </Link>
+                    </li>
+                </ul>
+            </footer>
+        </div>
+    );
+}
---
+++ react-springboot-luv2read/react-library/src/lib/oktaConfig.ts
@@ -0,0 +1,10 @@
+export const oktaConfig = {
+    clientId: '0oa8g95qahbcRapnh5d7',
+    issuer: 'https://dev-48402033.okta.com/oauth2/default',
+    redirectUri: 'https://book-search-d54b3.web.app/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true,
+}
+
+
